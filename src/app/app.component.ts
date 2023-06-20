1cb111cfafb323bd15b05348af131af4fcd43b1e
iTzMiNOS <amirerf1998@gmail.com>
2023-06-20 21:54:40 +0300

    modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	modified:   src/index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularForm';
+  onSubmit(ftmp: NgForm){
+    console.log(ftmp.value);
+
+  }
+  getValue(f: any){
+    console.log(f);
+
+  }
 }
